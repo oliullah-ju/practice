780afbeb00ec70b9b75faedcffc7623a44f07299
oliullah-ju <oli.it.ju@gmail.com>
2017-01-11 22:41:02 +0600

    Add files via upload

---
+++ course_login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Online Attendance login</title>
+		<link rel="stylesheet" type="text/css" href="login.css">
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" type="text/css" href="clog.css"/>
+		
+	</head>
+	
+	<body>
+	<div class="fix main">
+		<div class="fix header">
+			<img src="PAN.jpg" alt=""/>
+		</div>
+		
+		<div class="manue">
+			<ul>
+					<li><a href="">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropbtn">Attendance</a>
+						<div class="dropdown-content">
+							<a href="#">4th Batch</a>
+							<a href="#">5th Batch</a>
+							<a href="#">6th Batch</a>
+							<a href="#">7th Batch</a>
+						</div>
+					</li>
+					<li><a href="">Info</a></li>
+					<li><a href="">Member</a></li>
+					<li><a href="">Result</a></li>
+					<li><a href="">Contact us</a></li>
+					
+				</ul>
+		
+		</div>
+		
+		
+		<div class="container">
+			<img src="login.png">
+			<form>
+				<div class="form-input">
+					<input type="text" name="username" placeholder="Enter your username">
+				</div>
+				<div class ="form-input">
+					<input type="password" name="password" placeholder="Enter your password">
+				</div>
+					<input type="submit" name="submit" value="login">
+			</form>
+		</div>
+		<div class="fix footer">
+					<p>@copy: IIT, Jahangirnagar University, All Rights Reserved.</p>
+		</div>
+		
+	</div>
+		
+
+	
+	</body>
+</html>
---
+++ main.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Online Attendance Management System</title>
+		<link rel="stylesheet" type="text/css" href="login.css">
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" type="text/css" href="css/bar/bar.css"/>
+		<link rel="stylesheet" type="text/css" href="css/dark/dark.css"/>
+		<link rel="stylesheet" type="text/css" href="css/light/light.css"/>
+		<link rel="stylesheet" type="text/css" href="css/default/default.css"/>
+		<link rel="stylesheet" type="text/css" href="css/nivo-slider.css"/>
+	</head>
+	
+	<body>
+	<div class="fix main">
+		<div class="fix header">
+			<img src="PAN.jpg" alt=""/>
+		</div>
+		
+		
+		<div class="manue">
+			<ul>
+					<li><a href="">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropbtn">Attendance</a>
+						<div class="dropdown-content">
+							<a href="#">4th Batch</a>
+							<a href="course_login.php">5th Batch</a>
+							<a href="#">6th Batch</a>
+							<a href="#">7th Batch</a>
+						</div>
+					</li>
+					<li><a href="">Info</a></li>
+					<li><a href="">Member</a></li>
+					<li><a href="">Result</a></li>
+					<li><a href="">Contact us</a></li>
+					
+				</ul>
+		
+		</div>
+		
+		<div class="oli">
+		
+		</div>
+		<div class="fix slider">
+			<div class="slider-wrapper theme-default">
+				<div  class="nivoSlider" id="slider">
+					<img src="images/1.jpg" alt=""/>
+					<img src="images/sc1.png" alt=""/>
+					<img src="images/2.jpg" alt=""/>
+					<img src="images/3.jpg" alt=""/>
+					<img src="images/4.jpg" alt=""/>
+					
+					
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="fix maincontent">
+		
+		</div>
+		<div class="fix content">
+			<h2><u>IIT Programs</u></h2><br>
+			<p>Programs offered by IIT aims at developing up-to-date human resources in the field of Information Technology. </p>
+		    <p>Through the delivery of state-of-art teaching and practical insights, these programs will ultimately endeavor to</p> 
+			<p>create the IT specialists who will lead our nation as well as the world.</p>
+			<br><br>
+			<h2><u>BSC(Hons) in IT </u></h2><br>
+			<p>B.Sc. (Hons.) in Information Technology shall extend over a period of four academic years and will consist of eight semesters.</p>
+			<p>The program is hereinafter called Undergraduate Program. Each year will divide into two semesters. Each semester will have the</p> 
+			<p>pduration of six months.Students shall be evaluated in each semester. A semester will be segmented into Class-weeks, Preparatory</p>
+			<p>leave and Semester-end examination.</p>
+			
+			
+			<br><br>
+			<h2><u>M.Sc in IT </u></h2><br>
+			<p>M. Sc. in Information Technology shall extend over a period of one academic years and will consist of two semesters.</p> 
+			<p>The program is hereinafter called Graduate Program. Each semester will have the duration of six months.Students shall</p> 
+			<p>be evaluated in each semester. A semester will be segmented into Class-weeks, Preparatory leave and Semester-end examination.</p>
+			</div>
+			
+			
+	<div class="fix footer">
+				<p>@copy: IIT, Jahangirnagar University, All Rights Reserved.</p>
+				
+	</div>
+		</div>
+		
+<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+	<script type="text/javascript">
+	$(window).load(function() {
+	$('#slider').nivoSlider();
+	});
+	</script>
+	
+	</body>
+</html>
